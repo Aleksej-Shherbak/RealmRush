60b822009bacc1f0c255e8cd911fa2ff72d664d8
Щербак Алексей <aleksej.shherbak@inbox.ru>
2023-05-24 19:16:17 +0300

    Finished damaging enemies.

---
+++ Assets/Enemy/EnemyHealth.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private int maxHitPoints = 5;
+    private int currentHitPoints = 0;
+
+    void Start()
+    {
+        currentHitPoints = maxHitPoints;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+
+    void ProcessHit()
+    {
+        currentHitPoints--;
+        
+        if (currentHitPoints <= 0)
+            Destroy(gameObject);
+    }
+}
