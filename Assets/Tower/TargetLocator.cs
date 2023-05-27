b463c77b657632e08c1b9ba268ad60d272ee508f
Щербак Алексей <aleksej.shherbak@inbox.ru>
2023-05-27 19:08:08 +0300

    Added: shoot in closest enemies only.

--- Assets/Tower/TargetLocator.cs
+++ Assets/Tower/TargetLocator.cs
@@ -3,21 +3,47 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] private Transform weapon;
+    [SerializeField] private float range = 15f;
+    [SerializeField] private ParticleSystem projectileParticle;
     private Transform target;
     
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-
-    }
-
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
     }
 
-    private void AimWeapon()
+    void FindClosestTarget()
     {
+        // TODO refactor this later 
+        var enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach (var enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+
+        target = closestTarget;
+    }
+
+    void AimWeapon()
+    {
+        float targetDistance = Vector3.Distance(transform.position, target.position);
+        Attack(targetDistance <= range);
         weapon.LookAt(target);
+    }
+
+    void Attack(bool isActive)
+    {
+        var projectileParticleEmission = projectileParticle.emission;
+        projectileParticleEmission.enabled = isActive;
     }
 }
